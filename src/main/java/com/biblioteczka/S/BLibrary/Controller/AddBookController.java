12b063d0f877f52bbd03fd97632bf80a54f6c46b
Damian Sawera <damian.sawera@unit4.com>
2023-02-07 18:29:27 +0100

    Swagger implementation

--- src/main/java/com/biblioteczka/S/BLibrary/Controller/AddBookController.java
+++ src/main/java/com/biblioteczka/S/BLibrary/Controller/AddBookController.java
@@ -3,16 +3,19 @@ package com.biblioteczka.S.BLibrary.Controller;
 
 import com.biblioteczka.S.BLibrary.Model.BookModel;
 import com.biblioteczka.S.BLibrary.Service.BookServiceImpl;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/add-book")
 public class AddBookController {
     private BookServiceImpl bookService;
+
+    public AddBookController(BookServiceImpl bookService) {
+        this.bookService = bookService;
+    }
 
     @GetMapping
     public String home(){
