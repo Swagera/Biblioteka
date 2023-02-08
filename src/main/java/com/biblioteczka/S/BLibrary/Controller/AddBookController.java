a34680c95ab6c9e0cb83a2d0d1a4ca0f84e6ae2c
Damian Sawera <damian.sawera@unit4.com>
2023-02-08 08:26:04 +0100

    Swagger implementation, update book template, all crud methods

--- src/main/java/com/biblioteczka/S/BLibrary/Controller/AddBookController.java
+++ src/main/java/com/biblioteczka/S/BLibrary/Controller/AddBookController.java
@@ -5,10 +5,12 @@ import com.biblioteczka.S.BLibrary.Model.BookModel;
 import com.biblioteczka.S.BLibrary.Service.BookServiceImpl;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.persistence.Table;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @RequestMapping("/add-book")
 public class AddBookController {
     private BookServiceImpl bookService;
@@ -18,14 +20,16 @@ public class AddBookController {
     }
 
     @GetMapping
-    public String home(){
+    public String home(Model model){
+        model.addAttribute("book",new BookModel());
         return "add-book";
     }
 
 
     @PostMapping
-    public String addBook(@RequestBody BookModel Book) {
-        bookService.createBook(Book);
-        return "add-book";
+    public String addBook(@ModelAttribute BookModel bookModel, Model model) {
+        model.addAttribute("book", bookModel);
+        bookService.createBook(bookModel);
+        return "redirect:/book-list";
     }
 }
--- src/main/java/com/biblioteczka/S/BLibrary/Controller/SearchController.java
+++ src/main/java/com/biblioteczka/S/BLibrary/Controller/SearchController.java
@@ -1,17 +1,23 @@
 package com.biblioteczka.S.BLibrary.Controller;
 
 
+import com.biblioteczka.S.BLibrary.Service.BookServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping("/search")
 public class SearchController {
+    @Autowired
+private BookServiceImpl bookService;
 
     @GetMapping
-    public String search(){
-        return "search";
+    public String listBooks(Model model) {
+        model.addAttribute("books", bookService.getAllBooks());
+        return "/search";
     }
-
 }
